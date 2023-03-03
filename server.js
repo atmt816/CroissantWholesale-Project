514fbaf189a37a6a5054a040a1054c8d592d6c73
misaelh2015 <61474910+misaelh2015@users.noreply.github.com>
2023-03-02 21:52:51 -0600

    Set up Frontend Node.js server

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 // load the things we need
 var express = require('express');
 var app = express();
-const bodyParser  = require('body-parser');
+const bodyParser = require('body-parser');
 // required module to make calls to a REST API
 const axios = require('axios');
 const { response } = require('express');
 const req = require('express/lib/request');
 var selectedID = "";
-//app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded());
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 
     res.render('pages/index');
 });
+
+app.listen(5000);
+console.log('5000 is the magic port');
