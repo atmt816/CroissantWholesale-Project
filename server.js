d1f48815f4ec0365c29794c168799a5a88428c36
atmt816 <70179093+atmt816@users.noreply.github.com>
2023-03-02 19:25:37 -0600

    WIP Index and Login

---
+++ server.js
@@ -0,0 +1,17 @@
+// load the things we need
+var express = require('express');
+var app = express();
+const bodyParser  = require('body-parser');
+// required module to make calls to a REST API
+const axios = require('axios');
+const { response } = require('express');
+const req = require('express/lib/request');
+var selectedID = "";
+//app.use(bodyParser.urlencoded());
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+
+    res.render('pages/index');
+});
