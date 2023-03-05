6b4eedc5510b89f4ac6cba906cb89ad1909e5d51
misaelh2015 <61474910+misaelh2015@users.noreply.github.com>
2023-03-05 15:58:37 -0600

    mysql connector added

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+import mysql.connector
+from mysql.connector import Error
 import hashlib
 import datetime
 import time
@@ -8,6 +10,23 @@ from flask import request, make_response
 # from sql import execute_read_query
 # from sql import execute_query
 from datetime import datetime
+
+
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occured.")
+    return connection
+
 
 # setting up the application
 app = flask.Flask(__name__)  # sets up the application
