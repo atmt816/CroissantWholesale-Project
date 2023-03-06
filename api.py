090fa2fa16ac4990835f8a604dc8219686103c1b
misaelh2015 <61474910+misaelh2015@users.noreply.github.com>
2023-03-06 03:43:44 -0600

    Get method for employees added

--- api.py
+++ api.py
@@ -58,32 +58,44 @@ def execute_read_query(connection, query):
 app = flask.Flask(__name__)  # sets up the application
 app.config["DEBUG"] = True  # allow to show error in browser
 
-@app.route('/employees', methods = ['GET'])
+
+@app.route('/employee', methods=['GET'])
 def employees():
-    # try: 
+    # try:
     #     token = current_tokens[curuser.get_current_user()]
     # except:
     #     return 'No Active User Detected'
     # if float(token) > time.time():
-        sql = """
+    sql = """
         SELECT e.first_name, e.last_name, e.start_date, e.end_date, e.emp_status, r.role_name
         FROM employees e
         JOIN roles r
         ON e.role_id = r.role_id;
         """
-        results = execute_read_query(conn, sql)
+    results = execute_read_query(conn, sql)
 
-        sql = """
+    sql = """
         SELECT * FROM States
         """
-        state_results = execute_read_query(conn, sql)
-        return jsonify(state_results, results)
+    state_results = execute_read_query(conn, sql)
+    return jsonify(state_results, results)
+
+# employees get method working now
+# not returning data for now since roles table is empty
+
 
 @app.route('/employees', methods=['GET'])
-def get_trip():
+def get_employees():
     conn = create_connection(
-        'cis4375.cfab8c2lm5ph.us-east-1.rds.amazonaws.com', 'admin', 'cougarcode', 'cis4375')
-    sql = "SELECT * FROM employees"
+        'cis4375.cfab8c2lm5ph.us-east-1.rds.amazonaws.com', 'admin', 'cougarcode', 'cis4375db')
+    # sql = "SELECT * FROM employees"
+    sql = """
+        SELECT e.first_name, e.last_name, e.start_date, e.end_date, e.emp_status, r.role_name
+        FROM employees AS e
+        JOIN roles AS r
+        ON e.role_id = r.role_id;
+        """
+    # sql = "SELECT * FROM states"
     employees = execute_read_query(conn, sql)
     return employees
 
