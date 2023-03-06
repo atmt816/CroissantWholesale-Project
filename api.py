f5bace7cc08956d2e6a777f719a1a1978b6459d8
atmt816 <70179093+atmt816@users.noreply.github.com>
2023-03-05 22:08:22 -0600

    Create employee form ex

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ import time
 import flask  # , werkzeug
 from flask import jsonify, make_response
 from flask import request, make_response
+from flask import request, jsonify
 # from sql import create_connection
 # from sql import execute_read_query
 # from sql import execute_query
@@ -32,5 +33,25 @@ def create_connection(host_name, user_name, user_password, db_name):
 app = flask.Flask(__name__)  # sets up the application
 app.config["DEBUG"] = True  # allow to show error in browser
 
+@app.route('/employees', methods = ['GET'])
+def employees():
+    # try: 
+    #     token = current_tokens[curuser.get_current_user()]
+    # except:
+    #     return 'No Active User Detected'
+    # if float(token) > time.time():
+        sql = """
+        SELECT e.first_name, e.last_name, e.start_date, e.end_date, e.emp_status, r.role_name
+        FROM employees e
+        JOIN roles r
+        ON e.role_id = r.role_id;
+        """
+        results = execute_read_query(conn, sql)
+
+        sql = """
+        SELECT * FROM States
+        """
+        state_results = execute_read_query(conn, sql)
+        return jsonify(state_results, results)
 
 app.run()
