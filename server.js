ae41f86c1cd2900c95411bf6cec254e014c9f7ac
atmt816 <70179093+atmt816@users.noreply.github.com>
2023-03-08 18:54:48 -0600

    Employee Information

--- server.js
+++ server.js
@@ -26,19 +26,21 @@ app.get('/login', function (req, res) {
 
 app.get('/employees', function(req, res){
     axios.get('http://127.0.0.1:5000/employees')
-    .then((response)=>{
+    .then((response, states)=>{
+        console.log(response.data)
         var employee_data = response.data
 
         res.render('pages/employees', 
-        {employee_data: employee_data });
-    
+        { employee_data: employee_data
+        //   states: employee_data[1]  
+          });
     });
 });
 
-// app.get('/employees', function (req, res) {
-
-//     res.render('pages/employees');
-// });
+app.get('/empinfo', function (req, res) {
+    axios.get('http://127.0.0.1:5000/employee_info')
+    res.render('pages/empinfo');
+});
 
 
 app.listen(3000);
