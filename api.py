a1d375bfea64836a7374dca486fb3e143fe8b8f3
atmt816 <70179093+atmt816@users.noreply.github.com>
2023-03-08 20:10:42 -0600

    Connection Errors (Emp)

--- api.py
+++ api.py
@@ -47,14 +47,18 @@ def execute_query(connection, query):
 
 
 def execute_read_query(connection, query):
-    cursor = connection.cursor(dictionary=True)
-    result = None
+    cursor = connection.cursor()
+    result = []
     try:
         cursor.execute(query)
-        result = cursor.fetchall()
-        return jsonify(result)
+        table = cursor.fetchall()
+        columnNames = [column[0] for column in cursor.description]
+        for record in table:
+            result.append(dict(zip(columnNames, record )))
+        return result
     except Error as e:
         print(f"The error '{e}' occured")
+
 
 # setting up the application
 app = flask.Flask(__name__)  # sets up the application
@@ -79,8 +83,13 @@ def employee_info():
         """
     employees = execute_read_query(conn, sql)
 
+    sql = """
+        SELECT * FROM states;
+        """ 
+    states = execute_read_query(conn, sql)
 
-    return employees
+    return jsonify(employees, states)
+
 
 
 # employee_contact get method working now
