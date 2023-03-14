665b51a8ba19cfa446bf52914ce8ae82c6860c8c
misaelh2015 <61474910+misaelh2015@users.noreply.github.com>
2023-03-14 17:29:12 -0500

    /empinfo work in progress

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // load the things we need
 var express = require('express');
 var app = express();
-const bodyParser  = require('body-parser');
+const bodyParser = require('body-parser');
 // required module to make calls to a REST API
 const axios = require('axios');
 const { response } = require('express');
@@ -26,42 +26,56 @@ app.get('/login', function (req, res) {
 
 // EMPLOYEE PAGE 
 
-app.get('/employees', function(req, res){
+app.get('/employees', function (req, res) {
     axios.get('http://127.0.0.1:5000/employees')
-    .then((response, states)=>{
-        console.log(response.data)
-        var employee_data = response.data
+        .then((response, states) => {
+            console.log(response.data)
+            var employee_data = response.data
 
-        res.render('pages/employees', 
-        { employee_data: employee_data[0],
-          states: employee_data[1],
-          roles: employee_data[2]   
-          });
-    });
+            res.render('pages/employees',
+                {
+                    employee_data: employee_data[0],
+                    states: employee_data[1],
+                    roles: employee_data[2]
+                });
+        });
 });
 
-app.get('/empinfo/:id', function (req, res) {
-    const emp_id = req.query.id;
-    axios.get('http://127.0.0.1:5000/empinfo',
-    )
-  
-    res.render('pages/empinfo', 
-    {employee_data: employee_data, emp_id: emp_id });
+app.get('/empinfo', function (req, res) {
+    /*const emp_id = req.query.id;*/
+    axios.get('http://127.0.0.1:5000/empinfo')
+        .then((response) => {
+            var emp_id = req.query.emp_ids;
+            var employee_data = response.data
+            var empinfo = employee_data[0].emp_id;
+            console.log(empinfo)
+
+            res.render('pages/empinfo', {
+                employee_data: employee_data[0]
+            });
+        });
+
+
+    /*res.render('pages/empinfo',
+        { employee_data: employee_data, emp_id: emp_id });
+*/
+
 });
 
 // CUSTOMER PAGE
 
 
-app.get('/customers', function(req, res){
+app.get('/customers', function (req, res) {
     axios.get('http://127.0.0.1:5000/customers')
-    .then((response, states)=>{
-        console.log(response.data)
-        var customer_data = response.data
+        .then((response, states) => {
+            console.log(response.data)
+            var customer_data = response.data
 
-        res.render('pages/customers', 
-        { customer_data : customer_data   
-          });
-    });
+            res.render('pages/customers',
+                {
+                    customer_data: customer_data
+                });
+        });
 });
 
 app.get('/custinfo', function (req, res) {
