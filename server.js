ebf14d57df7c3d5c928d1402dafbcf6babfe3506
misaelh2015 <61474910+misaelh2015@users.noreply.github.com>
2023-03-15 16:51:52 -0500

    added, employee_data parsing for states and roles

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // load the things we need
 var express = require('express');
 var app = express();
-const bodyParser  = require('body-parser');
+const bodyParser = require('body-parser');
 // required module to make calls to a REST API
 const axios = require('axios');
 const { response } = require('express');
@@ -26,72 +26,77 @@ app.get('/login', function (req, res) {
 
 // EMPLOYEE PAGE 
 
-app.get('/employees', function(req, res){
+app.get('/employees', function (req, res) {
     axios.get('http://127.0.0.1:5000/employees')
-    .then((response, states)=>{
-        // console.log(response.data)
-        var employee_data = response.data
+        .then((response, states) => {
+            // console.log(response.data)
+            var employee_data = response.data
 
-        res.render('pages/employees', 
-        { employee_data: employee_data[0],
-          states: employee_data[1],
-          roles: employee_data[2]   
-          });
-    });
+            res.render('pages/employees',
+                {
+                    employee_data: employee_data[0],
+                    states: employee_data[1],
+                    roles: employee_data[2]
+                });
+        });
 });
 
 
 app.get('/empinfo/:id', function (req, res) {
     const emp_id = req.params.id;
-    
-    axios.get('http://127.0.0.1:5000/empinfo/'+ emp_id
-    ).then((response, states)=>{
+
+    axios.get('http://127.0.0.1:5000/empinfo/' + emp_id
+    ).then((response, states) => {
         var employee_data = response.data
-        
-        res.render('pages/empinfo', 
-        {employee_data: employee_data[0]
-        });
+
+        res.render('pages/empinfo',
+            {
+                employee_data: employee_data[0],
+                states: employee_data[1],
+                roles: employee_data[2]
+            });
     });
-  
-    
+
+
 });
 
 
-app.post('/employees/add', function(req, res) {
+app.post('/employees/add', function (req, res) {
     axios.post('http://127.0.0.1:5000/employees/add',
-     {
-        first_name : req.body.first_name,
-        last_name : req.body.last_name,
-        start_date: req.body.start_date,
-        emp_status : req.body.emp_status,
-        role_id: req.body.role_id,
-        phone: req.body.phone,
-        email: req.body.email,
-        street: req.body.street,
-        city: req.body.city,
-        state_code_id: req.body.states,
-        zipcode: req.body.zipcode
-     }
+        {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            start_date: req.body.start_date,
+            emp_status: req.body.emp_status,
+            role_id: req.body.role_id,
+            phone: req.body.phone,
+            email: req.body.email,
+            street: req.body.street,
+            city: req.body.city,
+            state_code_id: req.body.states,
+            zipcode: req.body.zipcode
+        }
     )
-    .then((response)=>{
-        
-        res.render('pages/employees');
-        
-    });
+        .then((response) => {
+
+            res.render('pages/employees');
+
+        });
 });
 // CUSTOMER PAGE
 
 
-app.get('/customers', function(req, res){
+app.get('/customers', function (req, res) {
     axios.get('http://127.0.0.1:5000/customers')
-    .then((response, states)=>{
-        console.log(response.data)
-        var customer_data = response.data
+        .then((response, states) => {
+            console.log(response.data)
+            var customer_data = response.data
 
-        res.render('pages/customers', 
-        { customer_data : customer_data   
-          });
-    });
+            res.render('pages/customers',
+                {
+                    customer_data: customer_data
+                });
+        });
 });
 
 app.get('/custinfo', function (req, res) {
