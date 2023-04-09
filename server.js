919882f15541ba5b87a67a1f4c7c5b7314240014
atmt816 <70179093+atmt816@users.noreply.github.com>
2023-04-09 14:42:34 -0500

    order changes wip

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.post('/employees/add', function (req, res) {
                             employee_data: employee_data[0],
                             states: employee_data[1],
                             roles: employee_data[2],
-                            success: 'The new employee has been added.'
+                            success: 'The employee has been added.'
                         });
                 });
         });
@@ -346,7 +346,7 @@ app.post('/customers/add', function (req, res) {
                         {
                             customer_data: customer_data[0],
                             states: customer_data[1],
-                            success: 'The new customer has been added.'
+                            success: 'The customer has been added.'
                         });
                 });
         });
@@ -449,7 +449,7 @@ app.post('/vendors/add', function (req, res) {
                             vendor_data: vendor_data[0],
                             states: vendor_data[1],
                             roles: vendor_data[2],
-                            success: 'The new vendor has been added.'
+                            success: 'The vendor has been added.'
                         });
                 });
         });
@@ -498,7 +498,6 @@ app.get('/inventory', function (req, res) {
     axios.get('http://127.0.0.1:5000/inventory')
         .then((response) => {
             var inventory_data = response.data
-            
             res.render('pages/inventory',
                 {
                     inventory_data: inventory_data[0],
@@ -528,7 +527,8 @@ app.post('/inventory/add', function (req, res) {
                     res.render('pages/inventory',
                         {
                             inventory_data: inventory_data[0],
-                            vendors: inventory_data[1]
+                            vendors: inventory_data[1],
+                            success: 'The inventory log has been updated.'
 
                         });
                 });
@@ -541,7 +541,7 @@ app.get('/inveninfo/:id', function (req, res) {
     axios.get('http://127.0.0.1:5000/inveninfo/' + inventory_id
     ).then((response, states) => {
         var inventory_data = response.data
-        console.log(inventory_data[0].inventory_id)
+        console.log(inventory_data)
         res.render('pages/inveninfo',
             {
                 inventory_data: inventory_data[0],
@@ -575,7 +575,8 @@ app.post('/inventory/update', function (req, res) {
                     res.render('pages/inventory',
                         {
                             inventory_data: inventory_data[0],
-                            vendors: inventory_data[1]
+                            vendors: inventory_data[1],
+                            success: 'The inventory log has been updated.'
 
                         });
                 });
@@ -585,7 +586,7 @@ app.post('/inventory/update', function (req, res) {
 });
 
 app.post('/inventory/delete', function (req, res) {
-    axios.delete('http://127.0.0.1:5000/delete_inventory',
+    axios.put('http://127.0.0.1:5000/delete_inventory',
         {
             inventory_id: req.body.inventory_id
 
@@ -600,7 +601,8 @@ app.post('/inventory/delete', function (req, res) {
                     res.render('pages/inventory',
                         {
                             inventory_data: inventory_data[0],
-                            vendors: inventory_data[1]
+                            vendors: inventory_data[1],
+                            success: 'The inventory log has been deleted.'
 
                         });
                 });
@@ -753,7 +755,7 @@ app.get('/maintenanceinfo/:id', function (req, res) {
     axios.get('http://127.0.0.1:5000/maintenance/' + log_id
     ).then((response, states) => {
         var logs_data = response.data
-
+        
         res.render('pages/maintenanceinfo',
             {
                 logs_data: logs_data
@@ -1186,7 +1188,8 @@ app.post('/orders/add', function (req, res) {
                             order_data: order_data[0],
                             customers: order_data[1],
                             products: order_data[2],
-                            line_items: order_data[3]
+                            line_items: order_data[3],
+                            success: 'The order has been added.'
                         });
                 });
         });
@@ -1240,7 +1243,8 @@ app.post('/orders/update', function (req, res) {
                             order_data: order_data[0],
                             customers: order_data[1],
                             products: order_data[2],
-                            line_items: order_data[3]
+                            line_items: order_data[3],
+                            success: 'The order has been updated.'
                         });
                 });
         }
@@ -1263,7 +1267,8 @@ app.post('/orders/delete', function (req, res) {
                         order_data: order_data[0],
                         customers: order_data[1],
                         products: order_data[2],
-                        line_items: order_data[3]
+                        line_items: order_data[3],
+                        success: 'The order has been deleted.'
                     });
             });
     }
